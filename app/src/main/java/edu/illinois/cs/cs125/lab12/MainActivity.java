d3ee10c70a114bdfac97c5813657bc99e0917719
Qingying Zheng <qzheng13@illinois.edu>
2018-04-25 10:07:31 -0500

    updates from qing

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageButton;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,11 +24,11 @@ import java.io.IOException;
 import java.util.Arrays;
 
 /**
- * Main class for our UI design lab.
+ * Main class for our UI design .
  */
 public final class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
-    private static final String TAG = "Lab12:Main";
+    private static final String TAG = "MP7:Main";
 
     /** Request queue for our API requests. */
     private static RequestQueue requestQueue;
@@ -48,11 +50,39 @@ public final class MainActivity extends AppCompatActivity {
         // Create button
         final Button refreshButton = findViewById(R.id.go_button);
 
+        /*
+         * Set up handlers for each button in our UI. These run when the buttons are clicked.
+         */
+        final Button go_button = findViewById(R.id.go_button);
+        go_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Open file button clicked");
+                TextView textView = findViewById(R.id.text);
+                textView.setText("Hello bitch");
+                textView.setVisibility(View.VISIBLE);
+                //startGo_button();
+            }
+        });
+
+        /**
+         * Make the go_button do something .
+         */
+//        private void startGo_button() {
+//            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+//            intent.addCategory(Intent.CATEGORY_OPENABLE);
+//            intent.setType("image/*");
+//            startActivityForResult(intent, READ_REQUEST_CODE);
+//        }
+
+
+
         //specify an action when the button is pressed,
         // set a click listener on the button object in the corresponding activity code:
         refreshButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(final View v) {
                 // Code here executes on main thread after user presses button
+
 
             }
         });
